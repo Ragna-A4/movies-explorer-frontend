9fa5dc35df7b174770ad2e50cc68788f4b977604
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-09-06 14:17:17 +0300

    build: add images, fonts, initial data with movies

---
+++ src/utils/data.js
@@ -0,0 +1,111 @@
+const wordsImage = new URL(
+  "../images/initialdata/33words.jpg",
+  import.meta.url
+);
+const yearsImage = new URL(
+  "../images/initialdata/100years.jpg",
+  import.meta.url
+);
+const banksyImage = new URL(
+  "../images/initialdata/banksy.jpg",
+  import.meta.url
+);
+const basquiatImage = new URL(
+  "../images/initialdata/basquiat.jpg",
+  import.meta.url
+);
+const booksellersImage = new URL(
+  "../images/initialdata/booksellers.jpg",
+  import.meta.url
+);
+const gimmeImage = new URL(
+  "../images/initialdata/gimme.jpg",
+  import.meta.url
+);
+const janiceImage = new URL(
+  "../images/initialdata/janice.jpg",
+  import.meta.url
+);
+const onthewavesImage = new URL(
+  "../images/initialdata/onthewaves.jpg",
+  import.meta.url
+);
+const pjhurvyImage = new URL(
+  "../images/initialdata/pjhurvy.jpg",
+  import.meta.url
+);
+const pullyourselfImage = new URL(
+  "../images/initialdata/pullyourself.jpg",
+  import.meta.url
+);
+const runningImage = new URL(
+  "../images/initialdata/running.jpg",
+  import.meta.url
+);
+const thinkingImage = new URL(
+  "../images/initialdata/thinking.jpg",
+  import.meta.url
+);
+
+export const initialData = [
+  {
+    name: "33 слова о дизайне",
+    duration: "177",
+    image: wordsImage,
+  },
+  {
+    name: "Киноальманах «100 лет дизайна»",
+    duration: "57",
+    image: yearsImage,
+  },
+  {
+    name: "В погоне за Бенкси",
+    duration: "712",
+    image: banksyImage,
+  },
+  {
+    name: "Баския: Взрыв реальности",
+    duration: "77",
+    image: basquiatImage,
+  },
+  {
+    name: "Бег это свобода",
+    duration: "92",
+    image: runningImage,
+  },
+  {
+    name: "Книготорговцы",
+    duration: "60",
+    image: booksellersImage,
+  },
+  {
+    name: "Когда я думаю о Германии ночью",
+    duration: "33",
+    image: thinkingImage,
+  },
+  {
+    name: "Gimme Danger: История Игги и The Stooges",
+    duration: "215",
+    image: gimmeImage,
+  },
+  {
+    name: "Дженис: Маленькая девочка грустит",
+    duration: "37",
+    image: janiceImage,
+  },
+  {
+    name: "Соберись перед прыжком",
+    duration: "177",
+    image: pullyourselfImage,
+  },
+  {
+    name: "Пи Джей Харви: A dog called money",
+    duration: "87",
+    image: pjhurvyImage,
+  },
+  {
+    name: "По волнам: Искусство звука в кино",
+    duration: "91",
+    image: onthewavesImage,
+  },
+];
