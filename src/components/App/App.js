0bb7893377b854fcc16ca48c5154e91a5f114826
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-09-11 20:32:41 +0300

    feat: add promo component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,16 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import NotFound from "../NotFound/NotFound";
-import Header from "../Header/Header";
-import HeaderMain from "../Header/HeaderMain";
-import Footer from "../Footer/Footer";
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="app">
       <div className="page">
-        <HeaderMain />
-        <Header />
         <Routes>
+          <Route path='/' element={<Main />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import HeaderMain from '../Header/HeaderMain';
+import Promo from './Promo/Promo';
+import Footer from '../Footer/Footer';
+
+function Main() {
+  return (
+    <>
+    <HeaderMain />
+    <Promo />
+    <Footer />
+    </>
+  );
+}
+
+export default Main;
