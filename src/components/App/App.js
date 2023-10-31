2ee98749fd49d42907732d39fae7601e8de66d24
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-10-31 13:34:12 +0300

    feat: add protectedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,16 +9,34 @@ import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
+import { ProtectedRoute } from "../ProtectedRoute";
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = React.useState(false);
+
   return (
     <div className="app">
       <div className="page">
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/saved-movies" element={<SavedMovies />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/movies"
+            element={
+              <ProtectedRoute element={Movies} isLoggedIn={isLoggedIn} />
+            }
+          />
+          <Route
+            path="/saved-movies"
+            element={
+              <ProtectedRoute element={SavedMovies} isLoggedIn={isLoggedIn} />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute element={Profile} isLoggedIn={isLoggedIn} />
+            }
+          />
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="*" element={<NotFound />} />
