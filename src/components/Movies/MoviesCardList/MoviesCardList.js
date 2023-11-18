bfeabca2bde6aaeda04bdd974fd06eed28bdc7c8
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-11-18 20:22:35 +0300

    feat: add moviesCounter

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,17 +1,23 @@
 import React from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import { useWidth, useMoviesCounter } from "../../../utils/MoviesCounter";
 
 import "./MoviesCardList.css";
 
 function MoviesCardList(props) {
+
+  const width = useWidth();
+  const { moviesCounter, addMovies } = useMoviesCounter(width);
+
+
   return (
     <section className="moviescardlist">
       <div className="moviescardlist__container">
-        {props.movies.map(({ id, ...props }) => (
+        {props.movies.slice(0, moviesCounter).map(({ id, ...props }) => (
           <MoviesCard key={id} {...props} />
         ))}
       </div>
-      <button className="moviescardlist__button" type="button">
+      <button className="moviescardlist__button" type="button" onClick={addMovies}>
         Ещё
       </button>
     </section>
---
+++ src/utils/MoviesCounter.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export function useWidth() {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  const getWindowWidth = () => {
+    const newWidth = window.innerWidth;
+    setWindowWidth(newWidth);
+  };
+
+  const handleResize = () => {
+    setTimeout(getWindowWidth, 1000);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return windowWidth;
+}
+
+export function useMoviesCounter(windowWidth) {
+  const [moviesCounter, setMoviesCounter] = React.useState(0);
+  const [moviesToAdd, setMoviesToAdd] = React.useState(0);
+
+  React.useEffect(() => {
+    function updateMoviesCounter() {
+      if (windowWidth >= 1280) {
+        setMoviesCounter(12);
+        setMoviesToAdd(3);
+        return;
+      } else if (windowWidth >= 768) {
+        setMoviesCounter(8);
+        setMoviesToAdd(2);
+        return;
+      } else {
+        setMoviesCounter(5);
+        setMoviesToAdd(2);
+        return;
+      }
+    }
+    updateMoviesCounter();
+  }, [windowWidth]);
+
+  function addMovies() {
+    setMoviesCounter(moviesCounter + moviesToAdd);
+  }
+
+  return { moviesCounter, addMovies };
+}
