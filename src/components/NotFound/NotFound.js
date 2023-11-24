1fb1fd2cbbf17b2e96477af70a398a12d447ce04
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-11-24 11:01:00 +0300

    feat: use navigate at notfound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-      <main className="notfound">
-        <h1 className="notfound__title">404</h1>
-        <p className="notfound__subtitle">Страница не найдена</p>
-        <Link to="" className="notfound__link">
-          Назад
-        </Link>
-      </main>
+    <main className="notfound">
+      <h1 className="notfound__title">404</h1>
+      <p className="notfound__subtitle">Страница не найдена</p>
+      <Link className="notfound__link" onClick={() => navigate(-2)}>
+        Назад
+      </Link>
+    </main>
   );
 }
 
