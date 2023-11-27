a841e3fbabf9ad9a35dc3c85de8bed8f229be8e8
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-11-27 13:15:03 +0300

    fix: show actual page in the authorized header menu

--- src/components/Header/HeaderAuthorized.js
+++ src/components/Header/HeaderAuthorized.js
@@ -29,7 +29,11 @@ function HeaderAuthorized() {
         <nav className="header__menu header__menu_type_authorized">
           <Link
             to={`${location.pathname === "/movies" ? "" : "/movies"}`}
-            className="header__menu-item header__menu-item_type_authorized"
+            className={`header__menu-item header__menu-item_type_authorized ${
+              location.pathname === "/movies"
+                ? "header__menu-item_isactive"
+                : ""
+            }`}
           >
             Фильмы
           </Link>
@@ -37,7 +41,11 @@ function HeaderAuthorized() {
             to={`${
               location.pathname === "/saved-movies" ? "" : "/saved-movies"
             }`}
-            className="header__menu-item header__menu-item_type_authorized"
+            className={`header__menu-item header__menu-item_type_authorized ${
+              location.pathname === "/saved-movies"
+                ? "header__menu-item_isactive"
+                : ""
+            }`}
           >
             Сохранённые фильмы
           </Link>
